6f51c120ab3eee45b04856282b0a84b16377cdcb
ysl0628 <yihsinlan@gmail.com>
2023-02-11 18:46:35 +0800

    feat: implement delete category and path filter

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -14,7 +14,19 @@ router.get('/archives', function (req, res, next) {
 })
 
 router.get('/categories', function (req, res, next) {
-  res.render('dashboard/categories', { title: 'Express' })
+  const messages = req.flash('info')
+  const errorMessages = req.flash('error')
+  console.log(messages)
+  categoriesRef.once('value', (snapshot) => {
+    const categories = snapshot.val()
+    res.render('dashboard/categories', {
+      title: '分類',
+      categories,
+      messages,
+      errorMessages,
+      hasInfo: messages.length > 0 || errorMessages.length > 0,
+    })
+  })
 })
 
 router.post('/categories/create', (req, res) => {
@@ -25,9 +37,27 @@ router.post('/categories/create', (req, res) => {
     ...data,
     id: key,
   }
-  categoryRef.set(category).then(() => {
-    res.redirect('/dashboard/categories')
-  })
+  categoriesRef
+    .orderByChild('path')
+    .equalTo(data.path)
+    .once('value')
+    .then((snapshot) => {
+      if (snapshot.val() !== null) {
+        req.flash('error', '路徑已存在，請重新輸入')
+        res.redirect('/dashboard/categories')
+      } else {
+        categoryRef.set(category).then(() => {
+          res.redirect('/dashboard/categories')
+        })
+      }
+    })
+})
+
+router.post('/categories/delete/:id', (req, res) => {
+  const id = req.params.id
+  categoriesRef.child(id).remove()
+  req.flash('info', '已刪除分類')
+  res.redirect('/dashboard/categories')
 })
 
 router.get('/signup', function (req, res, next) {
